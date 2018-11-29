7f3c24c3ae8ca70efb60cf3748d138be534d9863
FTI <FTI@192.168.20.112>
2018-11-29 15:15:39 -0200

    Commit pela Laura (usuario)

--- src/br/com/whitemarket/controller/ControllerUsuario.java
+++ src/br/com/whitemarket/controller/ControllerUsuario.java
@@ -5,9 +5,13 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import br.com.whitemarket.dao.JPAUsuarioDAO;
 import br.com.whitemarket.model.Usuario;
+import javax.servlet.http.HttpSession;
 
 
 @Controller
@@ -43,9 +47,35 @@ public class ControllerUsuario {
 	}
 	
 	@RequestMapping("/autenticacao")
-	public String auth() {
+	public String auth(@RequestParam String email, @RequestParam String senha, Model model, HttpSession session) {
+//		JPAUsuarioDAO user = new JPAUsuarioDAO();
+//		
+//		System.out.println("o que veio pelo form é: " + senha);
+//		user.autentica(email, senha);
 		
-		return "login";
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("market");
+		EntityManager	manager	= factory.createEntityManager();
+		Usuario usuario = new Usuario();
+		
+		System.out.println("entrou no controller com email = " + email);
+		
+		usuario = manager.createQuery(
+				  "SELECT u from Usuario u WHERE u.email = :email and u.senha = :senha", Usuario.class).
+				  setParameter("email", email).setParameter("senha", senha).getSingleResult();
+		
+		if(usuario.getCod_usuario() == 0) {
+			return "login";
+		}
+		
+		System.out.println("Retornou do banco usuario = " + usuario.getNome());
+		
+		model.addAttribute("usuarioLogado", usuario);
+		session.setAttribute("usuarioLogado",	usuario);
+		
+		manager.close();  
+		factory.close();
+		
+		return "redirect:telaPrincipal";
 	}
 
 }
