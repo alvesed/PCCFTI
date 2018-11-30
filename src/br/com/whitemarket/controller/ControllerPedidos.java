602a58c65e6b7c38c5a274535f5939160fe099ce
rafin <rafin@DESKTOP-7K7NJJ2>
2018-11-30 17:32:17 -0200

    fINAL 30-11

--- src/br/com/whitemarket/controller/ControllerPedidos.java
+++ src/br/com/whitemarket/controller/ControllerPedidos.java
@@ -67,7 +67,8 @@ public class ControllerPedidos {
 				   		" count(i.quantidade) as quantidades " + 
 				   		" FROM ItemPedido i "
 				   		+ " WHERE i.produto.codProduto = p.codProduto " + 
-				   		" ), dataCadastro, codProduto) from Produto p WHERE p.ativo = 1").getResultList();
+				   		" ), dataCadastro, codProduto) from Produto p  "
+				   		+ "WHERE p.ativo = 1 ").getResultList();
 		   
 		   for(Produto produto: listProdutos) {
 			   produto.setUrlPrimeiraImagem(util.pegarPrimeiraFoto(produto.getCodProduto()));
