c087105c9e11acceedcb11192e7c0635963d41e0
FTI <FTI@192.168.20.238>
2018-12-06 17:22:27 -0200

    PDF Finalizado. Salvando em C:/

--- src/br/com/whitemarket/controller/GerarPdfUtil.java
+++ src/br/com/whitemarket/controller/GerarPdfUtil.java
@@ -8,25 +8,28 @@ import java.util.List;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
 import com.itextpdf.text.Image;
 import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Phrase;
 import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.GrayColor;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
+import br.com.whitemarket.model.Endereco;
 import br.com.whitemarket.model.ItemPedido;
 import br.com.whitemarket.model.Pedido;
 import br.com.whitemarket.model.Produto;
 
 public class GerarPdfUtil {
 	public static void main(String[] args) {
-		gerarPdf(null);
+		gerarPdf(null, null);
 	}
 	
-	public static void gerarPdf(Pedido pedido) {
+	public static void gerarPdf(Pedido pedido, Endereco endereco) {
 		 // criação do documento
 	       Rectangle pageSize = new Rectangle(PageSize.A4);
 	       Document document = new Document(pageSize);
@@ -37,7 +40,7 @@ public class GerarPdfUtil {
 	           //p.setIndentationLeft(200);
 	           //document.add(p);
 	           //document.add(new Paragraph(" "));
-	           document.add(createTableUsuario(pedido));
+	           document.add(createTableUsuario(pedido, endereco));
 	           document.add(new Paragraph(" "));
 	           document.add(createTableProdutos(pedido));
 	           document.add(new Paragraph(" "));
@@ -52,26 +55,37 @@ public class GerarPdfUtil {
 	       document.close();
 	}
 	
-	public static PdfPTable createTableUsuario(Pedido pedido) {
-		// CRIA UMA TABELA COM 4 COLUNAS
-        PdfPTable table = new PdfPTable(4);
+	public static PdfPTable createTableUsuario(Pedido pedido, Endereco endereco) {
+		// CRIA UMA TABELA COM 3 COLUNAS
+		float[] columnWidths = {3, 5, 3};
+		PdfPTable table = new PdfPTable(columnWidths);
         table.setWidthPercentage(100);
         // CRIA UM OBJETO CELL
         PdfPCell cell;
         // ADICIONA AS COLUNAS
         if (pedido != null) {
 	        cell = new PdfPCell(new Phrase(pedido.getUsuario().getNome()));
-	        cell.setColspan(3);
+	        cell.setColspan(2);
 	        table.addCell(cell);
 	        cell = new PdfPCell(new Phrase("CPF: " + pedido.getUsuario().getCpf()));
 	        table.addCell(cell);
+	        //ROW 2
 	        cell = new PdfPCell(new Phrase("Pedido: N° " + pedido.getCod_pedido()));
 	        table.addCell(cell);
 	        cell = new PdfPCell(new Phrase("E-Mail: " + pedido.getUsuario().getEmail()));
-	        cell.setColspan(3);
+	        cell.setColspan(2);
 	        table.addCell(cell);
-	        cell = new PdfPCell(new Phrase("Endereço: " + pedido.getUsuario().getEndereco()));
-	        cell.setColspan(4);
+	        //ROW 3
+	        cell = new PdfPCell(new Phrase("Logradouro: " + endereco.getLogradouro()));
+	        cell.setColspan(2);
+	        table.addCell(cell);
+	        cell = new PdfPCell(new Phrase("CEP: " + endereco.getPostalCode()));
+	        table.addCell(cell);
+	        //ROW 4
+	        cell = new PdfPCell(new Phrase("Cidade: " + endereco.getCity()));
+	        cell.setColspan(2);
+	        table.addCell(cell);
+	        cell = new PdfPCell(new Phrase("Estado: " + endereco.getState()));
 	        table.addCell(cell);
         }
         return table;
@@ -79,34 +93,42 @@ public class GerarPdfUtil {
 	
 	public static PdfPTable createTableProdutos(Pedido pedido) {
 		// CRIA UMA TABELA COM 8 COLUNAS
-        PdfPTable table = new PdfPTable(9);
-        table.setTotalWidth(530);
-        table.setLockedWidth(true);
-        try {
-			table.setWidths(new float[] { 2, 1 });
-		} catch (DocumentException e1) {
-			e1.printStackTrace();
-		}
+		float[] columnWidths = {5, 3, 4, 7, 5, 5, 3, 5, 5};
+        PdfPTable table = new PdfPTable(columnWidths);
+        table.setWidthPercentage(100);
         // CRIA UM OBJETO CELL
         PdfPCell cell;
         // ADICIONA AS COLUNAS
         cell = new PdfPCell(new Phrase("Fotos"));
+        cell.setBackgroundColor(GrayColor.LIGHT_GRAY);
+        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
         table.addCell(cell);
-        cell = new PdfPCell(new Phrase("Código"));
+        cell = new PdfPCell(new Phrase("Cód."));
+        cell.setBackgroundColor(GrayColor.LIGHT_GRAY);
+        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
         table.addCell(cell);
-        cell = new PdfPCell(new Phrase("Produto"));
+        cell = new PdfPCell(new Phrase("Nome"));
+        cell.setBackgroundColor(GrayColor.LIGHT_GRAY);
         table.addCell(cell);
         cell = new PdfPCell(new Phrase("Descrição"));
+        cell.setBackgroundColor(GrayColor.LIGHT_GRAY);
         table.addCell(cell);
         cell = new PdfPCell(new Phrase("Condição"));
+        cell.setBackgroundColor(GrayColor.LIGHT_GRAY);
+        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
         table.addCell(cell);
         cell = new PdfPCell(new Phrase("Estado de Conserva."));
+        cell.setBackgroundColor(GrayColor.LIGHT_GRAY);
         table.addCell(cell);
         cell = new PdfPCell(new Phrase("Qtd."));
+        cell.setBackgroundColor(GrayColor.LIGHT_GRAY);
+        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
         table.addCell(cell);
         cell = new PdfPCell(new Phrase("Valor Unit."));
+        cell.setBackgroundColor(GrayColor.LIGHT_GRAY);
         table.addCell(cell);
         cell = new PdfPCell(new Phrase("Valor Total"));
+        cell.setBackgroundColor(GrayColor.LIGHT_GRAY);
         table.addCell(cell);
         // CRIA UMA LISTA DE PRODUTOS
         List<Produto> listaProdutos = new ArrayList<Produto>();
@@ -128,28 +150,36 @@ public class GerarPdfUtil {
 	            
 	            //CÓDIGO DO PRODUTO
 	            String codP = String.valueOf(item.getProduto().getCodProduto());
-	            table.addCell(codP);
+	            cell = new PdfPCell(new Phrase(codP));
+	            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+	            table.addCell(cell);
 	            table.addCell(item.getProduto().getNome());
 	            table.addCell(item.getProduto().getDescricao());
-	            table.addCell(item.getProduto().getCondicao());
+	            cell = new PdfPCell(new Phrase(item.getProduto().getCondicao()));
+	            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+	            table.addCell(cell);
 	            if (item.getProduto().getCondicao().equals("usado")) {
 		            table.addCell(item.getProduto().getEstadoProduto());
 	            } else {
-	            	table.addCell("N/A");
+	            	cell = new PdfPCell(new Phrase("-"));
+		            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+	            	table.addCell(cell);
 	            }
 	            
 	            //QUANTIDADE DE ITENS
 		        String qtd = String.valueOf(item.getQuantidade());
-		        table.addCell(qtd);
+		        cell = new PdfPCell(new Phrase(qtd));
+	            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+		        table.addCell(cell);
 	            
 	            //VALOR DO PRODUTO
 	            String valor = String.valueOf(item.getProduto().getValor());
-	            table.addCell(valor);
+	            table.addCell("R$ " + valor);
 	            
 	            //VALOR TOTAL DO PRODUTO
 	            BigDecimal valorTotal = item.getProduto().getValor().multiply(BigDecimal.valueOf(item.getQuantidade()));
 	            String valorTotalStr = String.valueOf(valorTotal);
-	            table.addCell(valorTotalStr);
+	            table.addCell("R$ " + valorTotalStr);
 	        }
         }
         return table;
@@ -161,10 +191,9 @@ public class GerarPdfUtil {
         PdfPCell cell;
         
         if (pedido != null) {
-        	cell = new PdfPCell(new Phrase("Valor Total dos Itens: " + pedido.getValor_pago()));
-        	table.addCell(cell);
-        } else {
-        	cell = new PdfPCell(new Phrase("Valor Total dos Itens: "));
+        	cell = new PdfPCell(new Phrase("Valor Total dos Itens: R$ " + pedido.getValor_pago()));
+        	cell.setBackgroundColor(GrayColor.LIGHT_GRAY);
+        	cell.setHorizontalAlignment(Element.ALIGN_CENTER);
         	table.addCell(cell);
         }
         
